90cf66d3249bf6b24fc572e18ad59a56c68afa06
Shashank <kiwimojito3012@proton.me>
2023-08-28 13:10:17 +0530

    WIP

--- xdp.h
+++ xdp.h
@@ -6,19 +6,3 @@
 #include <bpf_helpers.h>
 #include <bpf_endian.h>
 
-static inline __u16 csum_fold_helper(__u64 csum) {
-	for(int i = 0; i < 4; i++) {
-		if(csum >> 16) {
-			csum = (csum & 0xffff) + (csum >> 16);
-		}
-	}
-
-	return ~csum;
-}
-
-static inline __u16 iph_csum(struct iphdr* ipheader) {
-	ipheader->check = 0;
-	unsigned long long csum = bpf_csum_diff(0, 0, (unsigned int*)ipheader, sizeof(struct iphdr), 0);
-	
-	return csum_fold_helper(csum);	
-}
